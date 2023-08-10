e908941fa50bd4b486cf001a9826d2e10cba6ba1
Chamath Viduranga <chamath.viduranga@airudi.com>
2023-08-10 21:52:05 +0530

    initial commit

---
+++ Customer.Microservice/Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customer.Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private static List<Customer> _customers = new List<Customer>
+        {
+            new Customer { Id = 1, FirstName = "John", LastName = "Doe", Email = "john@example.com" },
+            new Customer { Id = 2, FirstName = "Jane", LastName = "Smith", Email = "jane@example.com" },
+            new Customer { Id = 3, FirstName = "Michael", LastName = "Johnson", Email = "michael@example.com" }
+        };
+
+        [HttpGet]
+        public IActionResult GetCustomers()
+        {
+            return Ok(_customers);
+        }
+    }
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Product.Microservice/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Product.Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public ProductController()
+        {
+                
+        }
+        private static List<Product> _products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product 1", Price = 10.99 },
+            new Product { Id = 2, Name = "Product 2", Price = 24.99 },
+            new Product { Id = 3, Name = "Product 3", Price = 7.49 }
+        };
+
+
+        [HttpGet]
+        public ActionResult GetProducts()
+        {
+            return Ok(_products);
+        }
+       
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
